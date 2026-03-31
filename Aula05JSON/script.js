1a999d5b8af968d4de4283143c91807ccacb2731
Janina de Siqueira <janinamarques92@gmail.com>
2026-03-31 12:08:27 -0300

    Aula05JSON

--- Aula05JSON/script.js
+++ Aula05JSON/script.js
@@ -23,3 +23,39 @@ function lerJSON(){
     req.open("GET", "dados.json" , true)
     req.send()
 }
+
+
+function lerProdutos(){
+    var req = new XMLHttpRequest()
+    
+
+    req.onreadystatechange = function(){
+        if( this.readyState == 4 && this.status == 200){
+            var objJSON = JSON.parse( this.responseText )
+            var produtos = objJSON.produtos
+            var txt = ""
+            if( produtos.lenght == 0 ){
+                txt = "<tr> <th>Nenhum produto cadastrado!</th></tr>"
+            }else{
+                txt = "<tr>"
+                txt +=    "   <th> Código </th>"
+                txt +=    "   <th> Nome </th>"
+                txt +=    "   <th> Preço </th>"
+                txt +=    "   </tr>"
+                produtos.forEach( prod => {
+                    txt += "<tr>"
+                    txt += "     <td>" + prod.id + "</td>"
+                    txt += "     <td>" + prod.nome + "</td>"
+                    txt += "     <td>" + prod.preco + "</td>"
+                    txt += "</tr>"
+                })
+                
+            }
+            document.getElementById("tblProdutos").innerHTML = txt
+        }
+
+
+    }
+    req.open( "GET" , "servidor.php?buscar", true)
+    req.send()
+}
